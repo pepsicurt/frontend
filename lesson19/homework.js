7aa9529a7de8089d3dd21f4fc19d939f7a33c24b
pepsicurt <abshahzoda49@gmail.com>
2023-12-07 16:17:06 +0500

    1homework

---
+++ lesson19/homework.js
@@ -0,0 +1,28 @@
+// let str1 = "Введите длину автомобиля"
+// let str2 =  "Введите объем двигателя"
+// let length = 2
+// let engine = 1
+
+// if( length <= 3.8 && engine <= 1.2){
+//     console.log("Class A")
+// } else if( length <= 4 && engine <= 1.6){
+//     console.log("Class B")
+// } else if( length <= 4.5 && engine == 1.6 && engine <= 2){
+//     console.log("Class C")
+// } else{
+//     console.log("Выберите автомобиль другого класса")
+// }
+
+
+
+let str = 'Введите текущий час'
+let hour = 19
+
+if( hour <= 11 ){
+    console.log("Доброе утро")
+} else if(hour >= 12  <=17 ){
+    console.log("Добрый день")
+} else if( hour >= 18  <= 23 ){
+    console.log("Добрый вечер")
+
+}
---
+++ lesson19/node.js
@@ -0,0 +1,111 @@
+//Number, string, boolean, underfined, object
+
+
+//number - числовые значения 
+//string - строки
+//boolean - булевые значения  правда(true) ложь(false)
+//underfined - означает что переменная не имеет значения
+//object - объекты
+
+//null - указывает на пустой объект (это не тип данных)
+
+
+//let a;
+
+
+//if(false) {
+    //a = 'string
+//}
+
+// console.log(a)
+
+//let num = 1333
+//num = 0.5
+
+//let str = " ", '', ``
+//console.log(`Hello ${num * 3}`)
+
+//let abc;
+// console.log(abc) == underfined
+
+
+
+
+// if (true){
+//   console.log
+//   var a = 15;
+//   console.log(a) === 15;
+//}
+
+
+
+// NaN - not a Number
+
+// let str  = '15la'
+// console.log(typeof str)
+
+// if (NaN === 15) {
+//     console.log(b)
+// } else {
+//     console.log("Hello")
+// }
+
+// let b = -Number.MAX_VALUE * 2 //-Infinity
+
+// console.log(b)
+
+
+// let i = 13
+// i--
+// i = i + 1
+// i = i - 1
+// console.log(i)
+
+
+// ==, ===
+//let a = '15'
+// let b = 15
+// if (a === b) {
+//     console.log("True")
+// } else{
+//     console.log("False")
+// }
+
+// !=, !===
+// >=, <=
+// > <
+
+
+// a ='15'
+// b = 15
+
+// if (a !== b ){
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+
+
+// let score = 64
+
+// if(score >= 84){
+//     console.log("Отлично")
+// } else if (score >= 64){
+//     console.log("Хорошо")
+// }else {
+//     console.log("Учись")
+// }
+
+
+
+
+// let a = 0
+
+// if(a >0){
+//     console.log("положительное")
+// } else if(a < 0){
+//     console.log("отрицательное")
+// } else{
+//     console.log(" не положительное и не отрицательное")
+// }
