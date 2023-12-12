b4099c9f16adcdac04b7e3dc2b2ad74dabe91d2a
pepsicurt <abshahzoda49@gmail.com>
2023-12-12 17:56:49 +0500

    lesson22.2

---
+++ lesson22/index.js
@@ -0,0 +1,112 @@
+// for (let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+
+// let a = 0;
+// while (a < 5){
+//     console.log(a)
+//     a++
+// }
+
+// let b = 6;
+// do{
+//     console.log(b)
+
+// } while(b < 5)
+
+
+
+
+
+// const usernames = ['@user1', '@user2', '@user3']
+
+
+// for(let i = 0; i < usernames.length; i++) {
+//     console.log(usernames[i])
+// }
+
+
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+
+
+
+// let nums = 2
+
+// if(nums %2 == 0){
+//     console.log("четн")
+// }else{
+//     console.log("нечетн")
+// }
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     if(i %2 == 0){
+//         console.log(i)
+//     }
+// }
+
+
+
+// let num = 10
+// let i = 1
+// while(i <= 1){
+//     console.log(num)
+//     num++
+// }
+
+
+
+
+// // Вывести все числа от 1 до числа которое ввел пользователь с помощю цикла while
+// let num = 10; // это число которое ввел пользователь
+// let i = 1; // переменная цикла
+// while (i <= num) { // цикл
+//     console.log(i) // выводим число
+//     i++ // прибавляем переменной 1
+// }
+
+// let num = 10
+// let i = 1;
+// while( i <= num ){
+//   if(i %2 == 0){
+//     console.log(i, "четн")
+//   }else{
+//     console.log(i, "нечетн")
+//   }
+//     i++
+// }
+
+
+
+// let num = 0
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+//     num = num + i
+// }
+// console.log(num)
+
+
+// let num = 0 //задаем значение переменной в которой содержится сумма всех чисел от 1 до 10 
+// let i = 1 //задаем значение переменной
+// while(i <= 10){ //это цикл, там условие что i меньше или равно 10
+//     num = num + i //решение, прибавляем сумме всех чисел от 1 до 10 i
+//     console.log(num) //выводим
+//     i++ //прибавляем 1 переменной 
+// }
+
+
+let num = 1
+let i = 1
+while(i <= 5){
+    num = num * i
+    console.log(num)
+    i++
+}
